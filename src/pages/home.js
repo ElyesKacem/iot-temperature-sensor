6c28060a66893fcd7c49801efc57626f385c53f6
elyes <elyeskacemeducation@gmail.com>
2023-05-10 01:40:10 +0100

    adding logout button

--- src/pages/home.js
+++ src/pages/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import "../App.css";
 
 import { useNavigate } from "react-router-dom";
+import { Button } from "@mui/material";
 function App() {
   const navigate = useNavigate();
 
@@ -20,6 +21,18 @@ function App() {
           style={{ border: "1px solid #cccccc" }}
           src="https://thingspeak.com/channels/2140743/charts/1?bgcolor=%23ffffff&color=%23d62020&dynamic=true&results=60&title=Courbe+de+temp%C3%A9rature&type=spline&xaxis=Temps&yaxis=Temp%C3%A9rature+%28C%C2%B0%29"
         ></iframe>
+        <br />
+        <br />
+        <br />
+        <Button
+          variant="contained"
+          onClick={() => {
+            localStorage.setItem("login", "");
+            navigate("/");
+          }}
+        >
+          Logout
+        </Button>
       </header>
     </div>
   );
