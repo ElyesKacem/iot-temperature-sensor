28af17c60d5262ef14e98a106d6b699faccba4e1
elyes <elyeskacemeducation@gmail.com>
2023-05-10 01:41:01 +0100

    changing email to username

--- src/pages/login.js
+++ src/pages/login.js
@@ -77,7 +77,7 @@ export default function Login() {
               required
               fullWidth
               id="email"
-              label="Email Address"
+              label="Username"
               name="email"
               autoComplete="email"
               autoFocus
