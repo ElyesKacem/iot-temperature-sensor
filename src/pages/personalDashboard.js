921801fc51c5e7b0345fdb613fc15ab6e1dac276
Mohamed Habaieb <mhabaieb24@gmail.com>
2023-05-11 01:50:05 +0100

    fix line chart dashboard

--- src/pages/personalDashboard.js
+++ src/pages/personalDashboard.js
@@ -1,107 +1,116 @@
 import React from "react";
-import { useNavigate } from "react-router-dom";
 import {
-  Chart as ChartJS,
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend,
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
 } from "chart.js";
 import { Line } from "react-chartjs-2";
-// import faker from "faker";
 import axios from "axios";
-import { Button } from "@mui/material";
+import Button from "@mui/material/Button";
+import { useNavigate } from "react-router-dom";
 
 ChartJS.register(
-  CategoryScale,
-  LinearScale,
-  PointElement,
-  LineElement,
-  Title,
-  Tooltip,
-  Legend
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
 );
 
 export const options = {
-  responsive: true,
-  plugins: {
-    legend: {
-      position: "top",
+    responsive: true,
+    plugins: {
+        legend: {
+            position: "top",
+        },
+        title: {
+            display: true,
+            text: "Temperature Sensor Dashboard",
+        },
     },
-    title: {
-      display: true,
-      text: "Chart.js Line Chart",
-    },
-  },
 };
 
 const labels = ["January", "February", "March", "April", "May", "June", "July"];
 
 export default function PersonalDashboard() {
-  const [dataCloud, setDataCloud] = React.useState([]);
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: "Temperature",
-        data: labels.map(() => dataCloud.field1),
-        borderColor: "rgb(255, 99, 132)",
-        backgroundColor: "rgba(255, 99, 132, 0.5)",
-      },
-    ],
-  };
-  const apiUrl =
-    "https://api.thingspeak.com/channels/2140743/feeds.json?results=8000";
-  const [counter, setCounter] = React.useState(0);
+    const [dataCloud, setDataCloud] = React.useState([]);
+    const [chartData, setChartData] = React.useState(null);
+    const navigate = useNavigate();
+    const apiUrl = "https://api.thingspeak.com/channels/2140743/feeds.json?results=8000";
 
-  React.useEffect(() => {
-    const timer = setTimeout(() => {
-      axios
-        .get(apiUrl)
-        .then((response) => {
-          console.log(response.data);
-          setDataCloud(response.data.feeds);
-        })
-        .catch((error) => {
-          console.error(error);
-        });
-      setCounter((prevCounter) => prevCounter + 1);
-      console.log(counter);
-    }, 2000);
+    const [counter, setCounter] = React.useState(0);
 
-    return () => {
-      clearTimeout(timer);
-    };
-  }, [counter]);
+    React.useEffect(() => {
+        const timer = setTimeout(() => {
+            setCounter((prevCounter) => prevCounter + 1);
+            console.log(counter);
+        }, 2000);
 
-  const navigate = useNavigate();
+        return () => {
+            clearTimeout(timer);
+        };
+    }, [counter]);
 
-  React.useEffect(() => {
-    if (!localStorage.getItem("login") || localStorage.getItem("login") == "") {
-      navigate("/iot-temperature-sensor/");
-    }
-  }, []);
+    React.useEffect(() => {
+        axios
+            .get(apiUrl)
+            .then((response) => {
+                console.log(response.data);
+                setDataCloud(response.data.feeds);
+            })
+            .catch((error) => {
+                console.error(error);
+            });
+    }, [counter]);
 
-  return (
-    <div>
-      {" "}
-      <Line options={options} data={data} />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Button
-        variant="contained"
-        onClick={() => {
-          localStorage.setItem("login", "");
-          navigate("/iot-temperature-sensor/");
-        }}
-      >
-        Logout
-      </Button>{" "}
-    </div>
-  );
+    React.useEffect(() => {
+        if (dataCloud.length > 0) {
+            const data = {
+                labels: dataCloud.map((item) => item.created_at),
+                datasets: [
+                    {
+                        label: "Temperature",
+                        data: dataCloud.map((item) => item.field1),
+                        borderColor: "rgb(255, 99, 132)",
+                        backgroundColor: "rgba(255, 99, 132, 0.5)",
+                    },
+                ],
+            };
+            setChartData(data);
+        }
+    }, [dataCloud]);
+
+    React.useEffect(() => {
+        if (!localStorage.getItem("login") || localStorage.getItem("login") === "") {
+            navigate("/iot-temperature-sensor/");
+        }
+    }, []);
+
+    return (
+        <>
+            <div style={{ width: "80%", height: "400px" }}>
+                {chartData && <Line options={options} data={chartData} />}
+            </div>
+            <br />
+            <br />
+            <br />
+            <br />
+            <Button
+                variant="contained"
+                onClick={() => {
+                    localStorage.setItem("login", "");
+                    navigate("/iot-temperature-sensor/");
+                }}
+            >
+                Logout
+            </Button>{" "}
+        </>
+    );
 }
